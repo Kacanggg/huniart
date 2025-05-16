1ec0f9394a3750a1013178b8c7dfa06c3bd03af2
Kacanggg <147565837+Kacanggg@users.noreply.github.com>
2025-05-16 21:11:23 +0700

    Update ButtonLink.jsx

--- src/components/button/ButtonLink.jsx
+++ src/components/button/ButtonLink.jsx
@@ -1,16 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const ButtonLink = ({ text = "Klik di sini", href = "/" }) => {
+const ButtonLink = ({ text = "Klik di sini", to = "/" }) => {
   return (
     <div className="flex justify-center mt-8">
-      <a
-        href={href}
-        rel="noopener noreferrer"
-        target="_blank"
-        className="px-6 py-3 bg-cta text-cta hover:bg-hover-dark text-white rounded-lg text-base font-medium transition duration-300"
+      <Link
+        to={to}
+        className="px-6 py-3 bg-cta hover:bg-hover-dark text-white rounded-lg text-base font-medium transition duration-300"
       >
         {text}
-      </a>
+      </Link>
     </div>
   );
 };
